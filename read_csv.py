75482a0c79fe0e5f87463087ada69415c74d0f2d
muraokazuya <murao.kazuya10@gmail.com>
2018-12-28 09:10:53 +0900

    Update read_csv.py

--- read_csv.py
+++ read_csv.py
@@ -2,5 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 a=np.loadtxt("filename", delimiter=",", encoding="sjis") #encoding must be your file encoding
+plt.subplot(2,3,1)
+plt.plot(a)
+plt.subplot(2,3,2)
 plt.plot(a)
 plt.show()
